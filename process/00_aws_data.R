5abf53cb84e68b1ef2d227109df3ff4ce5adabe9
adrhuerta <adrhuerta@gmail.com>
2021-11-17 12:45:36 -0500

    AWS >5%

--- process/00_aws_data.R
+++ process/00_aws_data.R
@@ -6,7 +6,13 @@ rm(list = ls())
 aws_data <- read.csv("data/raw/obs/AWS/AWS_data.csv", stringsAsFactors = FALSE)
 aws_data <- xts::xts(aws_data[,-1], as.POSIXct(aws_data[,1]))
 aws_data <- aws_data["2015/2020"]
+length_of_data <- apply(aws_data, 2, function(x){sum(!is.na(x))*100/length(x)})
+length_of_data <- length_of_data[length_of_data > 5]
+aws_data <- aws_data[, names(length_of_data)]
+
 aws_xyz <- read.csv("data/raw/obs/AWS/AWS_xyz.csv", stringsAsFactors = FALSE)
+aws_xyz <- aws_xyz[match(names(length_of_data), aws_xyz$CODE), ]
+row.names(aws_xyz) <- NULL
 
 # spatial xyz data
 sp_xyz <- sp::SpatialPointsDataFrame(coords = aws_xyz[, c("LON", "LAT")],
