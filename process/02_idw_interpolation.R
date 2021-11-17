62711a927cc2b0d666c29ab8ae919f2a7b44cd97
adrHuerta <adrhuerta@gmail.com>
2021-11-16 21:54:40 -0500

    adding 2 daily files

--- process/02_idw_interpolation.R
+++ process/02_idw_interpolation.R
@@ -27,3 +27,35 @@ parallel::mclapply(1:nrow(aws_data$values), function(time_step){
                         datatype = 'FLT4S', force_v4 = TRUE, compression = 7)
   
   }, mc.cores = 10)
+
+####
+
+reticulate::use_virtualenv("/home/waldo/PycharmProjects/forR/venv/", required = TRUE)
+reticulate::repl_python()
+
+import numpy as np
+import pandas as pd
+import xarray as xr
+import glob
+from joblib import Parallel, delayed
+
+PISCOpd = sorted(glob.glob("data/processed/gridded/PISCOpd/*.nc"))[1:]
+PISCOpd = xr.concat([xr.open_dataset(grid) for grid in PISCOpd], dim="time")
+time_range = (pd.date_range(start = "2015-01-01", periods = len(PISCOpd.time), freq = "D")).strftime("%Y-%m-%d")
+encoding = {v: {'zlib': True, 'complevel': 5} for v in ["p"]}
+
+def getting_hourly_files(step_time):
+  # from mean_SATc
+  # hourly files
+  hourly_files = sorted(glob.glob("data/processed/gridded/AWSs/AWSs_" + step_time + "*.nc"))
+  hourly_files_dates = pd.to_datetime([text.split("/")[-1].split("_")[-1].split(".nc")[0] for text in hourly_files], format="%Y-%m-%d %H:%M:%S")
+  hourly_files = [xr.open_dataset(grid) for grid in hourly_files]
+  # hourly 2 daily
+  hourly_files = np.round(xr.concat(hourly_files, dim="time").drop("crs"), 1)
+  hourly_files["time"] = hourly_files_dates
+  hourly_files.to_netcdf("data/processed/gridded/AWSs_by_day/AWSs_" + step_time + ".nc", encoding=encoding, engine='netcdf4')
+  
+
+Parallel(n_jobs=1, verbose=50)(
+  delayed(getting_hourly_files)(i) for i in time_range
+  )
