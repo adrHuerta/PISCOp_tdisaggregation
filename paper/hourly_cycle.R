3473b770726aa69be60d3eae5923dda9e7e37642
adrHuerta <adrhuerta@gmail.com>
2021-11-16 21:54:13 -0500

    hourly cycle figure

--- paper/hourly_cycle.R
+++ paper/hourly_cycle.R
@@ -1,4 +1,4 @@
-reticulate::use_virtualenv("/home/adrian/Documents/Repos/wa_eps_budyko/venv/", required = TRUE)
+reticulate::use_virtualenv("/home/waldo/PycharmProjects/forR/venv/", required = TRUE)
 reticulate::repl_python()
 
 import numpy as np
@@ -22,24 +22,65 @@ def amplitude_cycle(x):
   res = np.max(x) - np.min(x)
   return res
 
-seasons = [["11", "12", "01", "02","03","04"]]
-for season in range(len(seasons)):
-  print(season)
-  files_grid_0 = sorted(glob.glob("data/processed/gridded/PISCOp_hourly/*-" + seasons[season][0] + "-*.nc"))
-  files_grid_1 = sorted(glob.glob("data/processed/gridded/PISCOp_hourly/*-" + seasons[season][1] + "-*.nc"))
-  files_grid_2 = sorted(glob.glob("data/processed/gridded/PISCOp_hourly/*-" + seasons[season][2] + "-*.nc"))
-  files_grid_3 = sorted(glob.glob("data/processed/gridded/PISCOp_hourly/*-" + seasons[season][3] + "-*.nc"))
-  files_grid_4 = sorted(glob.glob("data/processed/gridded/PISCOp_hourly/*-" + seasons[season][4] + "-*.nc"))
-  files_grid_5 = sorted(glob.glob("data/processed/gridded/PISCOp_hourly/*-" + seasons[season][5] + "-*.nc"))
-  files_grid = [*files_grid_0, *files_grid_1, *files_grid_2, *files_grid_3, *files_grid_4, *files_grid_5]
+seasons = [["11", "12", "01", "02", "03", "04"]]
+season = 0
   
-  merged_files_grid = cdo.cat(input=' '.join(files_grid))
-  merged_files_grid = xr.open_dataset(merged_files_grid, chunks={"latitude": 100, "longitude": 100})
-  cycle_grid = xr.apply_ufunc(hourly_cycle, merged_files_grid, input_core_dims=[['time']], output_core_dims=[['time2']], output_sizes = {"time2":24}, output_dtypes="float32", vectorize=True, dask = "parallelized")
-  cycle_grid.to_netcdf("paper/output/PISCOp_hourly_hourly_cycle_" + "_".join(seasons[season]) + ".nc")
-  cycle_grid = xr.open_dataset("paper/output/PISCOp_hourly_hourly_cycle_" + "_".join(seasons[season]) + ".nc")
-  xr.apply_ufunc(get_time_of_max_value, cycle_grid, input_core_dims=[['time2']], vectorize=True).to_netcdf("paper/output/PISCOp_hourly_hourly_cycle_max_value_" + "_".join(seasons[season]) + ".nc")
-  xr.apply_ufunc(amplitude_cycle, cycle_grid, input_core_dims=[['time2']], vectorize=True).to_netcdf("paper/output/PISCOp_hourly_hourly_cycle_amplitude_" + "_".join(seasons[season]) + ".nc")
+#
+files_grid_0 = sorted(glob.glob("data/processed/gridded/PISCOp_hourly/*-" + seasons[season][0] + "-*.nc"))
+files_grid_1 = sorted(glob.glob("data/processed/gridded/PISCOp_hourly/*-" + seasons[season][1] + "-*.nc"))
+files_grid_2 = sorted(glob.glob("data/processed/gridded/PISCOp_hourly/*-" + seasons[season][2] + "-*.nc"))
+files_grid_3 = sorted(glob.glob("data/processed/gridded/PISCOp_hourly/*-" + seasons[season][3] + "-*.nc"))
+files_grid_4 = sorted(glob.glob("data/processed/gridded/PISCOp_hourly/*-" + seasons[season][4] + "-*.nc"))
+files_grid_5 = sorted(glob.glob("data/processed/gridded/PISCOp_hourly/*-" + seasons[season][5] + "-*.nc"))
+files_grid = [*files_grid_0, *files_grid_1, *files_grid_2, *files_grid_3, *files_grid_4, *files_grid_5]
   
-  cdo.cleanTempDir()
+merged_files_grid = cdo.cat(input=' '.join(files_grid))
+merged_files_grid = xr.open_dataset(merged_files_grid, chunks={"latitude": 100, "longitude": 100})
+cycle_grid = xr.apply_ufunc(hourly_cycle, merged_files_grid, input_core_dims=[['time']], output_core_dims=[['time2']],
+                            output_sizes={"time2": 24}, output_dtypes="float32", vectorize=True, dask="parallelized")
+cycle_grid.to_netcdf("paper/output/PISCOp_hourly_hourly_cycle_" + "_".join(seasons[season]) + ".nc")
+cycle_grid = xr.open_dataset("paper/output/PISCOp_hourly_hourly_cycle_" + "_".join(seasons[season]) + ".nc")
+xr.apply_ufunc(get_time_of_max_value, cycle_grid, input_core_dims=[['time2']], vectorize=True).to_netcdf("paper/output/PISCOp_hourly_hourly_cycle_maxvalue_" + "_".join(seasons[season]) + ".nc")
+xr.apply_ufunc(amplitude_cycle, cycle_grid, input_core_dims=[['time2']], vectorize=True).to_netcdf("paper/output/PISCOp_hourly_hourly_cycle_amplitude_" + "_".join(seasons[season]) + ".nc")
 
+cdo.cleanTempDir()
+  
+#
+files_grid_0 = sorted(glob.glob("data/processed/gridded/PISCOp_hourly_mean_SAT/*-" + seasons[season][0] + "-*.nc"))
+files_grid_1 = sorted(glob.glob("data/processed/gridded/PISCOp_hourly_mean_SAT/*-" + seasons[season][1] + "-*.nc"))
+files_grid_2 = sorted(glob.glob("data/processed/gridded/PISCOp_hourly_mean_SAT/*-" + seasons[season][2] + "-*.nc"))
+files_grid_3 = sorted(glob.glob("data/processed/gridded/PISCOp_hourly_mean_SAT/*-" + seasons[season][3] + "-*.nc"))
+files_grid_4 = sorted(glob.glob("data/processed/gridded/PISCOp_hourly_mean_SAT/*-" + seasons[season][4] + "-*.nc"))
+files_grid_5 = sorted(glob.glob("data/processed/gridded/PISCOp_hourly_mean_SAT/*-" + seasons[season][5] + "-*.nc"))
+files_grid = [*files_grid_0, *files_grid_1, *files_grid_2, *files_grid_3, *files_grid_4, *files_grid_5]
+  
+merged_files_grid = cdo.cat(input=' '.join(files_grid))
+merged_files_grid = xr.open_dataset(merged_files_grid, chunks={"latitude": 100, "longitude": 100})
+cycle_grid = xr.apply_ufunc(hourly_cycle, merged_files_grid, input_core_dims=[['time']], output_core_dims=[['time2']],
+                            output_sizes={"time2": 24}, output_dtypes="float32", vectorize=True, dask="parallelized")
+cycle_grid.to_netcdf("paper/output/PISCOp_hourlymeanSAT_hourly_cycle_" + "_".join(seasons[season]) + ".nc")
+cycle_grid = xr.open_dataset("paper/output/PISCOp_hourlymeanSAT_hourly_cycle_" + "_".join(seasons[season]) + ".nc")
+xr.apply_ufunc(get_time_of_max_value, cycle_grid, input_core_dims=[['time2']], vectorize=True).to_netcdf("paper/output/PISCOp_hourlymeanSAT_hourly_cycle_maxvalue_" + "_".join(seasons[season]) + ".nc")
+xr.apply_ufunc(amplitude_cycle, cycle_grid, input_core_dims=[['time2']], vectorize=True).to_netcdf("paper/output/PISCOp_hourlymeanSAT_hourly_cycle_amplitude_" + "_".join(seasons[season]) + ".nc")
+  
+cdo.cleanTempDir()
+  
+#
+files_grid_0 = sorted(glob.glob("data/processed/gridded/AWSs_by_day/*-" + seasons[season][0] + "-*.nc"))
+files_grid_1 = sorted(glob.glob("data/processed/gridded/AWSs_by_day/*-" + seasons[season][1] + "-*.nc"))
+files_grid_2 = sorted(glob.glob("data/processed/gridded/AWSs_by_day/*-" + seasons[season][2] + "-*.nc"))
+files_grid_3 = sorted(glob.glob("data/processed/gridded/AWSs_by_day/*-" + seasons[season][3] + "-*.nc"))
+files_grid_4 = sorted(glob.glob("data/processed/gridded/AWSs_by_day/*-" + seasons[season][4] + "-*.nc"))
+files_grid_5 = sorted(glob.glob("data/processed/gridded/AWSs_by_day/*-" + seasons[season][5] + "-*.nc"))
+files_grid = [*files_grid_0, *files_grid_1, *files_grid_2, *files_grid_3, *files_grid_4, *files_grid_5]
+  
+merged_files_grid = cdo.cat(input=' '.join(files_grid))
+merged_files_grid = xr.open_dataset(merged_files_grid, chunks={"latitude": 100, "longitude": 100})
+cycle_grid = xr.apply_ufunc(hourly_cycle, merged_files_grid, input_core_dims=[['time']], output_core_dims=[['time2']],
+                            output_sizes={"time2": 24}, output_dtypes="float32", vectorize=True, dask="parallelized")
+cycle_grid.to_netcdf("paper/output/AWSs_hourly_hourly_cycle_" + "_".join(seasons[season]) + ".nc")
+cycle_grid = xr.open_dataset("paper/output/AWSs_hourly_hourly_cycle_" + "_".join(seasons[season]) + ".nc")
+xr.apply_ufunc(get_time_of_max_value, cycle_grid, input_core_dims=[['time2']], vectorize=True).to_netcdf("paper/output/AWSs_hourly_hourly_cycle_maxvalue_" + "_".join(seasons[season]) + ".nc")
+xr.apply_ufunc(amplitude_cycle, cycle_grid, input_core_dims=[['time2']], vectorize=True).to_netcdf("paper/output/AWSs_hourly_hourly_cycle_amplitude_" + "_".join(seasons[season]) + ".nc")
+  
+cdo.cleanTempDir()
