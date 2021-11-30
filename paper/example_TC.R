e655e432cd76d29ae90ef732baada050e6de38b4
adrhuerta <adrhuerta@gmail.com>
2021-11-30 11:50:07 -0500

    example log TC

---
+++ paper/example_TC.R
@@ -0,0 +1,95 @@
+rm(list = ls())
+"%>%" = magrittr::`%>%`
+
+library(xts)
+
+# gridded satellite data
+imerg_early_files <- dir("./data/processed/gridded/IMERG-Early",
+                         full.names = TRUE)[-1] %>%
+  lapply(function(z){
+    raster::brick(z)[5000] %>% as.numeric()
+  }) %>% do.call(c, .) %>%
+  xts::xts(., seq(as.POSIXct("2015-01-01 00:00:00"), as.POSIXct("2020-12-31 23:00:00"), by = "hour")) #%>%
+  #xts::apply.daily(sum)
+  
+persiann_css_files <- dir("./data/processed/gridded/PERSIANN-CSS/",
+                          full.names = TRUE)[-1] %>%
+  lapply(function(z){
+    raster::brick(z)[5000] %>% as.numeric()
+  }) %>% do.call(c, .) %>%
+  xts::xts(., seq(as.POSIXct("2015-01-01 00:00:00"), as.POSIXct("2020-12-31 23:00:00"), by = "hour")) #%>%
+#xts::apply.daily(sum)
+
+gsmap_op_files <- dir("./data/processed/gridded/GSMaP_op/",
+                      full.names = TRUE)[-1] %>%
+  lapply(function(z){
+    raster::brick(z)[5000] %>% as.numeric()
+  }) %>% do.call(c, .) %>%
+  xts::xts(., seq(as.POSIXct("2015-01-01 00:00:00"), as.POSIXct("2020-12-31 23:00:00"), by = "hour")) #%>%
+#xts::apply.daily(sum)
+
+########
+# applying merging TC by following the work of https://www.researchgate.net/publication/343115933_Precipitation_Merging_Based_on_the_Triple_Collocation_Method_Across_Mainland_China
+# applying log-RMSE
+# using all time serie
+
+# 1
+#r_imerg = log(mean(imerg_early_files)) + (imerg_early_files - mean(imerg_early_files))*(1/mean(imerg_early_files))
+r_imerg = log(imerg_early_files + .1)
+# 2
+#r_persiann = log(mean(persiann_css_files)) + (persiann_css_files - mean(persiann_css_files))*(1/mean(persiann_css_files))
+r_persiann = log(persiann_css_files + .1)
+# 3
+#r_gsmap = log(mean(gsmap_op_files)) + (gsmap_op_files - mean(gsmap_op_files))*(1/mean(gsmap_op_files))
+r_gsmap = log(gsmap_op_files + .1)
+
+# C1,1
+C_imerg_imerg = sum((r_imerg - mean(r_imerg))*(r_imerg - mean(r_imerg)))/length(r_gsmap)
+# C1,2
+C_imerg_persiann = sum((r_imerg - mean(r_imerg))*(r_persiann - mean(r_persiann)))/length(r_gsmap)
+# C1,3
+C_imerg_gsmap = sum((r_imerg - mean(r_imerg))*(r_gsmap - mean(r_gsmap)))/length(r_gsmap)
+# C2,3
+C_persiann_gsmap = sum((r_persiann - mean(r_persiann))*(r_gsmap - mean(r_gsmap)))/8760
+# C2,2
+C_persiann_persiann = sum((r_persiann - mean(r_persiann))*(r_persiann - mean(r_persiann)))/8760
+# C3,3
+C_gsmap_gsmap = sum((r_gsmap - mean(r_gsmap))*(r_gsmap - mean(r_gsmap)))/8760
+
+# var1
+var_imerg = C_imerg_imerg - (C_imerg_persiann*C_imerg_gsmap)/C_persiann_gsmap
+var_persiann = C_persiann_persiann - (C_imerg_persiann*C_persiann_gsmap)/C_imerg_gsmap
+var_gsmap = C_gsmap_gsmap - (C_imerg_gsmap*C_persiann_gsmap)/C_imerg_persiann
+
+#var_imerg_r = var_imerg*mean(imerg_early_files)
+#var_persiann_r = var_persiann*mean(persiann_css_files)
+#var_gsmap_r = var_gsmap*mean(gsmap_op_files)
+
+#VAR_imerg_2 = ((1/mean(imerg_early_files))^2)*(var_imerg_r^2)
+VAR_imerg_2 = var_imerg*var_imerg
+#VAR_persiann_2 = ((1/mean(persiann_css_files))^2)*(var_persiann_r^2)
+VAR_persiann_2 = var_persiann*var_persiann
+#VAR_gsmap_2 = ((1/mean(gsmap_op_files))^2)*(var_gsmap_r^2)
+VAR_gsmap_2 = var_gsmap*var_gsmap
+
+w_imerg = (VAR_persiann_2*VAR_gsmap_2)/(VAR_imerg_2*VAR_persiann_2 + VAR_persiann_2*VAR_gsmap_2 + VAR_imerg_2*VAR_gsmap_2)
+w_persiann = (VAR_imerg_2*VAR_gsmap_2)/(VAR_imerg_2*VAR_persiann_2 + VAR_persiann_2*VAR_gsmap_2 + VAR_imerg_2*VAR_gsmap_2)
+w_gsmap = (VAR_imerg_2*VAR_persiann_2)/(VAR_imerg_2*VAR_persiann_2 + VAR_persiann_2*VAR_gsmap_2 + VAR_imerg_2*VAR_gsmap_2)
+
+rm = w_imerg*r_imerg + w_persiann*r_persiann + w_gsmap*r_gsmap
+
+# log rm
+lattice::xyplot(cbind(rm, r_imerg, r_persiann, r_gsmap))
+# log rm -> pp 
+lattice::xyplot(round(exp(cbind(rm, r_imerg, r_persiann, r_gsmap))-.1, 1))
+
+# log rm -> pp vs mean 
+lattice::xyplot(cbind(round(exp(rm) - .1, 1), 
+                      round(apply(cbind(imerg_early_files, persiann_css_files, gsmap_op_files), 1, mean), 1)))
+zoo::coredata(cbind(round(exp(rm) - .1, 1), 
+                      round(apply(cbind(imerg_early_files, persiann_css_files, gsmap_op_files), 1, mean), 1))) %>% plot(xlab = "TC", ylab = "Promedio")
+
+
+# applying merging TC by following the work of https://www.researchgate.net/publication/343115933_Precipitation_Merging_Based_on_the_Triple_Collocation_Method_Across_Mainland_China
+# applying log-RMSE
+# using a chunk by day
