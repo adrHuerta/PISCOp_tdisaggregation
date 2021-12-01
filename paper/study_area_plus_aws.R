d284a78c6c7ca6b319de6c4cba3629abaf59e402
adrhuerta <adrhuerta@gmail.com>
2021-11-30 20:47:12 -0500

    adding dem figure

--- paper/study_area_plus_aws.R
+++ paper/study_area_plus_aws.R
@@ -16,15 +16,19 @@ shp_lakes = file.path(".", "data", "others", "Lagos_lagunas_Project.shp") %>%
   broom::tidy()
 
 shp_sa = file.path(".", "data", "others", "Sudamérica.shp") %>%
-  shapefile() %>%
-  broom::tidy()
+  shapefile()
 
 # data
 aws <- readRDS("./data/processed/obs/AWS/AWSs.RDS")
 piscop <- raster::brick(
   dir("./data/processed/gridded/PISCOpd", full.names = TRUE) %>%
   lapply(function(x) raster::calc(raster::brick(x), mean))
-  ) %>% raster::calc(., mean)
+  ) %>% raster::calc(., mean) %>%
+  raster::mask(., shp_sa[12,])
+dem_area <- raster::raster("./data/others/DEM.nc") %>%
+  raster::mask(., shp_sa[12, ])
+
+shp_sa <- shp_sa %>% broom::tidy()
 
 p3 <- ggplot() + 
   geom_raster(data = raster::as.data.frame(piscop, xy = TRUE) %>%
@@ -56,7 +60,7 @@ p3 <- ggplot() +
              aes(x = LON, y = LAT), colour = "red", shape = 19, size = 3, stroke = .7) + #f3
   geom_label_repel(data = as.data.frame(aws$xyz[aws$xyz@data$LEN_x > 0,]), 
                    aes(x = LON, y = LAT, label = ESTACION),
-                   fill = "white", size = 1.75,
+                   fill = "white", size = 1.95,
                    box.padding = 1, alpha = .8) +
   # scale_size_manual("Filtro 3", values = c(3, 1.5)) +
   # scale_colour_manual("Filtro 2", values = c("blue", "red")) +
@@ -71,20 +75,58 @@ p3 <- ggplot() +
   #                  box.padding = 0.01, alpha = .5,
 #                  min.segment.length = unit(0, 'lines'),
 #                  nudge_x = 1, nudge_y = 1) +
-coord_quickmap(expand = c(0, 0), ylim = c(-18.575, 0.1), xlim = c(-81.325, -68.25)) + 
+  coord_quickmap(expand = c(0, 0), ylim = c(-18.575, 0.1), xlim = c(-81.325, -68.25)) + 
   #coord_quickmap(expand = F, ylim = c(-18.5, -15), xlim = c(-77, -72)) + 
   labs(x = "", y = "") +
   #theme_linedraw() + 
   theme_bw() + 
-  theme(axis.title = element_text(size = 8.5),
+  theme(axis.title = element_blank(),
         # axis.title.x = element_text(size = 15),
         # axis.text.x = element_blank(),
         # axis.title.y = element_text(size = 15),
-        axis.text.y = element_text(angle = 90),
+        #axis.text.y = element_blank(),
         legend.box = 'vertical',
         legend.justification = c(0, 0), legend.position = c(0, 0),
         legend.background = element_blank())
 
+
+#
+p4 <- ggplot() + 
+  geom_raster(data = raster::as.data.frame(dem_area, xy = TRUE) %>%
+                .[complete.cases(.),],
+              aes(x = x, y = y, fill = DEM/1000), alpha = .8) + 
+  scale_fill_gradientn(colors = colorRampPalette(rev(ochRe::ochre_palettes$olsen_seq))(10)[4:10],
+                     na.value= "lightblue",
+                     "             Elevation (km)",
+                     guide = guide_colorbar(frame.colour = "black",
+                                            ticks.colour = "black",
+                                            title.position = "left",
+                                            barheight = 8,
+                                            title.theme = element_text(size = 9,
+                                                                       angle = 90,
+                                                                       vjust = 0.5))) +
+  # geom_polygon(data = shp_peru,
+  #              aes(x = long, y = lat, group = group),
+  #              fill = NA, colour = "gray40", size = 0.5) +
+  geom_polygon(data = shp_sa,
+               aes(x = long, y = lat, group = group),
+               fill = NA, colour = "gray40", size = 0.8) +
+  coord_quickmap(expand = c(0, 0), ylim = c(-18.575, 0.1), xlim = c(-81.325, -68.25)) + 
+  #coord_quickmap(expand = F, ylim = c(-18.5, -15), xlim = c(-77, -72)) + 
+  labs(x = "", y = "") +
+  #theme_linedraw() + 
+  theme_bw() + 
+  theme(axis.title = element_blank(),
+        #axis.title.x = element_text(size = 15),
+        # axis.text.x = element_blank(),
+        # axis.title.y = element_text(size = 15),
+        #axis.text.y = element_blank(),
+        legend.box = 'vertical',
+        legend.justification = c(0, 0), legend.position = c(0, 0),
+        legend.background = element_blank())
+
+cowplot::plot_grid(p4, p3, ncol = 2)
+
 ggsave(file.path(".", "paper", "output", "Fig_study_area_stations.jpg"),
        dpi = 300, scale = 1,
-       width = 4.5, height = 6, units = "in")
+       width = 8.5, height = 6, units = "in")
