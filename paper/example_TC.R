1f1751f54ab01fabcd3976ad42efe0f1d50cecee
adrhuerta <adrhuerta@gmail.com>
2021-12-14 11:23:13 -0500

    (not used)

--- paper/example_TC.R
+++ paper/example_TC.R
@@ -5,7 +5,7 @@ library(xts)
 
 # gridded satellite data
 imerg_early_files <- dir("./data/processed/gridded/IMERG-Early",
-                         full.names = TRUE)[-1] %>%
+                         full.names = TRUE) %>%
   lapply(function(z){
     raster::brick(z)[5000] %>% as.numeric()
   }) %>% do.call(c, .) %>%
@@ -13,7 +13,7 @@ imerg_early_files <- dir("./data/processed/gridded/IMERG-Early",
   #xts::apply.daily(sum)
   
 persiann_css_files <- dir("./data/processed/gridded/PERSIANN-CSS/",
-                          full.names = TRUE)[-1] %>%
+                          full.names = TRUE) %>%
   lapply(function(z){
     raster::brick(z)[5000] %>% as.numeric()
   }) %>% do.call(c, .) %>%
@@ -21,7 +21,7 @@ persiann_css_files <- dir("./data/processed/gridded/PERSIANN-CSS/",
 #xts::apply.daily(sum)
 
 gsmap_op_files <- dir("./data/processed/gridded/GSMaP_op/",
-                      full.names = TRUE)[-1] %>%
+                      full.names = TRUE) %>%
   lapply(function(z){
     raster::brick(z)[5000] %>% as.numeric()
   }) %>% do.call(c, .) %>%
@@ -56,26 +56,23 @@ C_persiann_persiann = sum((r_persiann - mean(r_persiann))*(r_persiann - mean(r_p
 # C3,3
 C_gsmap_gsmap = sum((r_gsmap - mean(r_gsmap))*(r_gsmap - mean(r_gsmap)))/length(r_gsmap)
 
-# var1
-var_imerg = C_imerg_imerg - (C_imerg_persiann*C_imerg_gsmap)/C_persiann_gsmap
-var_persiann = C_persiann_persiann - (C_imerg_persiann*C_persiann_gsmap)/C_imerg_gsmap
-var_gsmap = C_gsmap_gsmap - (C_imerg_gsmap*C_persiann_gsmap)/C_imerg_persiann
+# rmse
+# rmse1
+rmse_imerg2 = C_imerg_imerg - (C_imerg_persiann*C_imerg_gsmap)/C_persiann_gsmap
+# rmse2
+rmse_persiann2 = C_persiann_persiann - (C_imerg_persiann*C_persiann_gsmap)/C_imerg_gsmap
+# rmse3
+rmse_gsmap2 = C_gsmap_gsmap - (C_imerg_gsmap*C_persiann_gsmap)/C_imerg_persiann
 
-#var_imerg_r = var_imerg*mean(imerg_early_files)
-#var_persiann_r = var_persiann*mean(persiann_css_files)
-#var_gsmap_r = var_gsmap*mean(gsmap_op_files)
+# w
+# w1
+w_imerg = (rmse_persiann2*rmse_gsmap2)/(rmse_imerg2*rmse_persiann2 + rmse_persiann2*rmse_gsmap2 + rmse_imerg2*rmse_gsmap2)
+# w2
+w_persiann = (rmse_imerg2*rmse_gsmap2)/(rmse_imerg2*rmse_persiann2 + rmse_persiann2*rmse_gsmap2 + rmse_imerg2*rmse_gsmap2)
+# w3
+w_gsmap = (rmse_imerg2*rmse_persiann2)/(rmse_imerg2*rmse_persiann2 + rmse_persiann2*rmse_gsmap2 + rmse_imerg2*rmse_gsmap2)
 
-#VAR_imerg_2 = ((1/mean(imerg_early_files))^2)*(var_imerg_r^2)
-VAR_imerg_2 = var_imerg*var_imerg
-#VAR_persiann_2 = ((1/mean(persiann_css_files))^2)*(var_persiann_r^2)
-VAR_persiann_2 = var_persiann*var_persiann
-#VAR_gsmap_2 = ((1/mean(gsmap_op_files))^2)*(var_gsmap_r^2)
-VAR_gsmap_2 = var_gsmap*var_gsmap
-
-w_imerg = (VAR_persiann_2*VAR_gsmap_2)/(VAR_imerg_2*VAR_persiann_2 + VAR_persiann_2*VAR_gsmap_2 + VAR_imerg_2*VAR_gsmap_2)
-w_persiann = (VAR_imerg_2*VAR_gsmap_2)/(VAR_imerg_2*VAR_persiann_2 + VAR_persiann_2*VAR_gsmap_2 + VAR_imerg_2*VAR_gsmap_2)
-w_gsmap = (VAR_imerg_2*VAR_persiann_2)/(VAR_imerg_2*VAR_persiann_2 + VAR_persiann_2*VAR_gsmap_2 + VAR_imerg_2*VAR_gsmap_2)
-
+# tc merging
 rm = w_imerg*r_imerg + w_persiann*r_persiann + w_gsmap*r_gsmap
 
 # log rm
@@ -92,25 +89,25 @@ zoo::coredata(cbind(round(exp(rm) - .1, 1),
 
 # applying merging TC by following the work of https://www.researchgate.net/publication/343115933_Precipitation_Merging_Based_on_the_Triple_Collocation_Method_Across_Mainland_China
 # applying log-RMSE
-# using a chunk by day
-
+# using a chunk by month
+# by day neither week does not work good
 # creating time range
 time_range <- seq(as.POSIXct("2015-01-01 00:00:00"), as.POSIXct("2020-12-31 23:00:00"), by = "hour")
-dailyVar <- as.list(sort(unique(format(time_range, format = "%m-%d"))))
-dailyVar[[59]] <- unlist(dailyVar[59:60])
-dailyVar[[60]] <- NULL
+dailyVar <- as.list(sort(unique(format(time_range, format = "%m"))))
+#%dailyVar[[59]] <- unlist(dailyVar[59:60])
+#dailyVar[[60]] <- NULL
 
 lapply(dailyVar, function(m_d){
   
   # 1
   #r_imerg = log(mean(imerg_early_files)) + (imerg_early_files - mean(imerg_early_files))*(1/mean(imerg_early_files))
-  r_imerg = log(imerg_early_files[format(time(imerg_early_files), "%m-%d") %in% m_d] + .1)
+  r_imerg = log(imerg_early_files[format(time(imerg_early_files), "%m") %in% m_d] + .1)
   # 2
   #r_persiann = log(mean(persiann_css_files)) + (persiann_css_files - mean(persiann_css_files))*(1/mean(persiann_css_files))
-  r_persiann = log(persiann_css_files[format(time(persiann_css_files), "%m-%d") %in% m_d] + .1)
+  r_persiann = log(persiann_css_files[format(time(persiann_css_files), "%m") %in% m_d] + .1)
   # 3
   #r_gsmap = log(mean(gsmap_op_files)) + (gsmap_op_files - mean(gsmap_op_files))*(1/mean(gsmap_op_files))
-  r_gsmap = log(gsmap_op_files[format(time(gsmap_op_files), "%m-%d") %in% m_d] + .1)
+  r_gsmap = log(gsmap_op_files[format(time(gsmap_op_files), "%m") %in% m_d] + .1)
   
   # C1,1
   C_imerg_imerg = sum((r_imerg - mean(r_imerg))*(r_imerg - mean(r_imerg)))/length(r_gsmap)
@@ -125,20 +122,23 @@ lapply(dailyVar, function(m_d){
   # C3,3
   C_gsmap_gsmap = sum((r_gsmap - mean(r_gsmap))*(r_gsmap - mean(r_gsmap)))/length(r_gsmap)
   
+  # rmse
+  # rmse1
+  rmse_imerg2 = C_imerg_imerg - ((C_imerg_persiann*C_imerg_gsmap) + .1)/(C_persiann_gsmap + .1)
+  # rmse2
+  rmse_persiann2 = C_persiann_persiann - ((C_imerg_persiann*C_persiann_gsmap) + .1)/(C_imerg_gsmap + .1)
+  # rmse3
+  rmse_gsmap2 = C_gsmap_gsmap - ((C_imerg_gsmap*C_persiann_gsmap) + .1)/(C_imerg_persiann + .1)
   
-  # var1
-  var_imerg = C_imerg_imerg - (C_imerg_persiann*C_imerg_gsmap)/C_persiann_gsmap
-  var_persiann = C_persiann_persiann - (C_imerg_persiann*C_persiann_gsmap)/C_imerg_gsmap
-  var_gsmap = C_gsmap_gsmap - (C_imerg_gsmap*C_persiann_gsmap)/C_imerg_persiann
+  # w
+  # w1
+  w_imerg = (rmse_persiann2*rmse_gsmap2)/(rmse_imerg2*rmse_persiann2 + rmse_persiann2*rmse_gsmap2 + rmse_imerg2*rmse_gsmap2)
+  # w2
+  w_persiann = (rmse_imerg2*rmse_gsmap2)/(rmse_imerg2*rmse_persiann2 + rmse_persiann2*rmse_gsmap2 + rmse_imerg2*rmse_gsmap2)
+  # w3
+  w_gsmap = (rmse_imerg2*rmse_persiann2)/(rmse_imerg2*rmse_persiann2 + rmse_persiann2*rmse_gsmap2 + rmse_imerg2*rmse_gsmap2)
   
-  VAR_imerg_2 = var_imerg*var_imerg
-  VAR_persiann_2 = var_persiann*var_persiann
-  VAR_gsmap_2 = var_gsmap*var_gsmap
-  
-  w_imerg = (VAR_persiann_2*VAR_gsmap_2)/(VAR_imerg_2*VAR_persiann_2 + VAR_persiann_2*VAR_gsmap_2 + VAR_imerg_2*VAR_gsmap_2)
-  w_persiann = (VAR_imerg_2*VAR_gsmap_2)/(VAR_imerg_2*VAR_persiann_2 + VAR_persiann_2*VAR_gsmap_2 + VAR_imerg_2*VAR_gsmap_2)
-  w_gsmap = (VAR_imerg_2*VAR_persiann_2)/(VAR_imerg_2*VAR_persiann_2 + VAR_persiann_2*VAR_gsmap_2 + VAR_imerg_2*VAR_gsmap_2)
-  
+  # tc merging
   rm = w_imerg*r_imerg + w_persiann*r_persiann + w_gsmap*r_gsmap
   cbind(rm, r_imerg, r_persiann, r_gsmap)
   
