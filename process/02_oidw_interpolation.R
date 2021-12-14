7c2f3232b56e461a68071f0ca9e5755691540aa6
adrhuerta <adrhuerta@gmail.com>
2021-12-14 18:11:06 -0500

    AWSs by day only based on idw

--- process/02_oidw_interpolation.R
+++ process/02_oidw_interpolation.R
@@ -31,6 +31,28 @@ parallel::mclapply(1:nrow(aws_data$values), function(time_step){
   }, mc.cores = 12)
 
 ####
+####
+# for technical validation
+
+parallel::mclapply(1:nrow(aws_data$values), function(time_step){
+  
+  xyz_i <- aws_data$xyz
+  xyz_i@data$value <- as.numeric(aws_data$values[time_step, ])
+  xyz_i <- xyz_i[complete.cases(xyz_i@data), ]
+  
+  gs <- gstat::gstat(formula = value ~ 1, locations = xyz_i, set = list(idp = 2))
+  idw <- raster::interpolate(PISCOp_grid, gs)
+  
+  name_to_save <- format(time(aws_data$values[time_step, ]), "%Y-%m-%d %H:%M:%S")
+  
+  raster::writeRaster(x = idw,
+                      varname = "p",
+                      filename = file.path("data/processed/gridded/AWSs_idw",
+                                           sprintf("%s_%s.nc", "AWSs_idw",  name_to_save)),
+                      datatype = 'FLT4S', force_v4 = TRUE, compression = 7)
+  
+}, mc.cores = 12)
+
 
 reticulate::use_virtualenv("/home/waldo/PycharmProjects/forR/venv/", required = TRUE)
 reticulate::repl_python()
@@ -49,7 +71,7 @@ encoding = {v: {'zlib': True, 'complevel': 5} for v in ["p"]}
 def getting_hourly_files(step_time):
   # from mean_SATc
   # hourly files
-  hourly_files = sorted(glob.glob("data/processed/gridded/AWSs/AWSs_" + step_time + "*.nc"))
+  hourly_files = sorted(glob.glob("data/processed/gridded/AWSs_idw/AWSs_idw_" + step_time + "*.nc"))
   hourly_files_dates = pd.to_datetime([text.split("/")[-1].split("_")[-1].split(".nc")[0] for text in hourly_files], format="%Y-%m-%d %H:%M:%S")
   hourly_files = [xr.open_dataset(grid) for grid in hourly_files]
   # hourly 2 daily
